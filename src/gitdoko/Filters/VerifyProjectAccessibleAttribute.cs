cb1d2d64dc00779f665aca841c1b3917bf20ecce
Paul Chen <lancevdance@gmail.com>
2016-08-23 17:13:03 +0800

    Add project layout.

--- src/gitdoko/Filters/VerifyProjectAccessibleAttribute.cs
+++ src/gitdoko/Filters/VerifyProjectAccessibleAttribute.cs
@@ -44,17 +44,19 @@ namespace gitdoko.Filters
             {
                 var routeValues = context.RouteData.Values;
 
-                var projectQuery = from p in AppDb.Projects
-                                   where String.Compare(p.Creator.UserName, (string)routeValues[Key_ProjectOwner], true) == 0
-                                      && String.Compare(p.Name, (string)routeValues[Key_ProjectName], true) == 0
-                                   select p;
+                var projectQuery = from u in AppDb.Users
+                                   from p in AppDb.Projects
+                                   where u == p.Creator
+                                      && String.Equals(u.UserName, (string)routeValues[Key_ProjectOwner], StringComparison.OrdinalIgnoreCase)
+                                      && String.Equals(p.Name, (string)routeValues[Key_ProjectName], StringComparison.OrdinalIgnoreCase)
+                                   select new { Creator = u, Project = p };
 
                 var projects = await projectQuery.ToArrayAsync();
                 if ( projects.Length == 1 )
                 {
                     //TODO: if ( targetProject.UserRights ... )
 
-                    var targetProject = projects[0];
+                    var targetProject = projects[0].Project;
                     context.HttpContext.Items[HttpContextItemKey_VerifiedProject] = targetProject;
 
                     var projectParamName = context.ActionDescriptor.Parameters.FirstOrDefault(p => p.ParameterType == typeof(Project))?.Name;
