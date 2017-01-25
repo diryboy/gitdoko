eb5e25b8eb9ec2ef1ec5050002755df88bb57991
Paul Chen <lancevdance@gmail.com>
2017-01-25 22:38:16 +0800

    Enable custom build of AdminLTE and bootstrap css.

--- src/gitdoko/assets/gulpfile.js
+++ src/gitdoko/assets/gulpfile.js
@@ -1,34 +1,26 @@
 ﻿var gulp = require('gulp');
-var modify = require('gulp-modify-file');
 
-gulp.task('build_3rd_party', ['transform_3rd_party'], function () {
-});
+gulp.task('build_3rd_party', () => {
+    var modify = require('gulp-modify-file');
+    var less = require('gulp-less');
+    var Path = require('path');
 
-gulp.task('transform_3rd_party', function () {
-    var originalSrcFiles = [
-        'bootstrap/less/bootstrap.less',
-        'admin-lte/build/less/AdminLTE.less'
-    ];
-    for (var i = 0; i < originalSrcFiles.length; i++) {
-        let libFile = originalSrcFiles[i];
-        gulp.src('node_modules/' + libFile)
-            .pipe(modify(function (content, path, file) {
-
-                var libName = libFile.match(/(.*?)\//)[1];
-                var varName = libName + '-dir';
-
-                content = '@' + varName + ': "../../node_modules/' + libFile.match(/.*\//)[0] + '";\n' + content;
-                content = content.replace(/(@import url\()/g, '// $1');
-                content = content.replace(/(@import ")/g, '$1@{' + varName + '}');
-
-                var newBootstrapVars = '../bootstrap-variables.less'
-                content = content.replace('@{bootstrap-dir}variables.less', newBootstrapVars);
-                content = content.replace('../bootstrap-less/variables.less', newBootstrapVars);
-
-                content = content.replace('../bootstrap-less', '../../node_modules/bootstrap/less');
-
-                return content;
-            }))
-            .pipe(gulp.dest('3rd_party/src'));
-    }
+    gulp.src([
+        'node_modules/bootstrap/less/bootstrap.less',
+        'node_modules/admin-lte/build/less/AdminLTE.less'
+    ])
+    .pipe(modify((content, path, file) => {
+        var newBootstrapVars = 'bootstrap-variables.less';
+        if (path.indexOf('AdminLTE') > 0) {
+            content = content.replace('@import url(', '// @import url(')
+                             .replace('../bootstrap-less/variables.less', newBootstrapVars)
+        } else {
+            content = content.replace('variables.less', newBootstrapVars);
+        }
+        return content;
+    }))
+    .pipe(less({
+        paths: [Path.join(__dirname, '3rd_party/src')]
+    }))
+    .pipe(gulp.dest('3rd_party'));
 });
