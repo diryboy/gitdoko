86a23d331df9016f27a87a37de28abc76329ca13
Paul Chen <lancevdance@gmail.com>
2017-04-26 23:42:36 +0800

    Update SignIn view.

--- src/gitdoko/ViewModels/Account.cs
+++ src/gitdoko/ViewModels/Account.cs
@@ -12,6 +12,7 @@ namespace gitdoko.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
     }
 
